3723546f2973405d29ad5d43fc42e7a55ecf6485
Hector Jimenez <xmyoot@yahoo.com>
2024-02-29 21:12:23 +0000

    updated sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,7 +4,13 @@ import { NavLink } from 'react-router-dom'
 import logo from '../../assets/images/logo.png'
 import logoSubtitle from '../../assets/images/logoSubtitle.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import {
+  faHome,
+  faUser,
+  faEnvelope,
+  faLinkedin,
+  faGithub,
+} from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => {
   return (
@@ -34,6 +40,34 @@ const Sidebar = () => {
           <FontAwesomeIcon icon={faEnvelope} />
         </NavLink>
       </nav>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/slobodan-gaji%C4%87-006bb8b8/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/bobangajicsm"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
