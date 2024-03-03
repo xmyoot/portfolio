aaea58df893ec2f349ab76fd73e28088d09c46dd
xmyoot <xmyoot@yahoo.com>
2024-03-03 15:05:18 -0800

    added styles for homepage

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom'
 import logo from '../../assets/images/logo.png'
+import './index.scss'
+
 const Home = () => {
   return (
     <div className="container home-page">
