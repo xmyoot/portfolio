dfa47dbe69eaf71bb153d75b2f92d0f971252520
xmyoot <xmyoot@yahoo.com>
2024-03-03 16:24:40 -0800

    updated style files

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -44,17 +44,21 @@ const Home = () => {
           <span className={`${letterClass} _13`}>I</span>
           <span className={`${letterClass} _14`}>'m</span>
           <img src={logo} alt="JavaScript Developer Name, Web Developer Name" />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
+          <span>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+          </span>
           <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={22}
-          />
+          <span>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </span>
         </h1>
         <h2>Frontend Developer / React Expert</h2>
         <Link to="/contact" className="flat-button">
