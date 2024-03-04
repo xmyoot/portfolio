b10676b2b37e5f13e9614e98a88c7b86b6a8e23b
xmyoot <xmyoot@yahoo.com>
2024-03-03 17:09:57 -0800

    made minor fixes to Home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -48,21 +48,17 @@ const Home = () => {
             src={logo}
             alt="JavaScript Developer Name, Software Developer Name"
           />
-          <span>
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={nameArray}
-              idx={15}
-            />
-          </span>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={17}
+          />
           <br />
-          <span>
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={jobArray}
-              idx={22}
-            />
-          </span>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={17}
+          />
         </h1>
         <h2>Frontend Developer / React Expert</h2>
         <Link to="/contact" className="flat-button">
